798b7d05d87a17a21e8d46725c5b746d64e16a86
Martin Burger <mburger82@yahoo.de>
2024-09-05 07:48:56 +0200

    add header

--- src/main.c
+++ src/main.c
@@ -1,4 +1,12 @@
-//Empty Template. FreeRTOS running in background.
+/********************************************************************************************* */
+//    Eduboard2 ESP32-S3 empty Template
+//    Author: Martin Burger
+//    Juventus Technikerschule
+//    Version: 1.0.0
+//    
+//    This is a empty template. ESP-IDF/FreeRTOS is running in the background.
+//    No BSP included. Starting point to program the ESP32-S3 from scratch.
+/********************************************************************************************* */
 void app_main()
 {
     
